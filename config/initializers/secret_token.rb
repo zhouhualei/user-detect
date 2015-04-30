abcde752fca210547eadfced6613287c5be272b1
warren <zhouhualei@gmail.com>
2015-04-30 14:22:57 +0800

    add devise for user authentication

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UserDetect::Application.config.secret_key_base = 'f6965cbfe0ef5332a57c19c68a81fe82b295b9461229a299652573e513c734e5f2eef12630ee267a2e25f0d28e9d8720239ea9c662b45002c780dafa7bce349d'

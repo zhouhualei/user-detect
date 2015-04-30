abf52a021b3466c7de7e3f438dd14f83e581fb19
warren <zhouhualei@gmail.com>
2015-04-30 16:31:25 +0800

    change session store to Database

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # user-detect
 
-### User Detect
+### Features
     1. collect login time of guest user or member
     2. provide RESTFul API for user statistics
 
 ### RESTFul API
-    GET /users/number
+    GET /users/number.json
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-UserDetect::Application.config.session_store :cookie_store, key: '_user-detect_session'
+UserDetect::Application.config.session_store :active_record_store, key: '_user-detect_session'
